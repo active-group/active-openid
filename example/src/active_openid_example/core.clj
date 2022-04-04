14426948b0540ee23738dec8a74e2a933f1058ff
Marco Schneider <marco.schneider@active-group.de>
2022-04-04 09:16:31 +0200

    Update example.

--- example/src/active_openid_example/core.clj
+++ example/src/active_openid_example/core.clj
@@ -1,24 +1,27 @@
+
 (ns active-openid-example.core
-  (:require [active.clojure.record :refer [define-record-type]]
-            [ring.adapter.jetty :as jetty]
+  (:require [active.clojure.config :as active-config]
             [active.clojure.lens :as lens]
-            [ring.util.response :as response]
             [active.clojure.logger.event :as log]
             [active.clojure.logger.log4j :as log4j]
-            [clojure.edn :as edn]
             [active.clojure.oidc :as oidc]
-            [hiccup.page :as hp]
-            [reitit.ring :as rr]
-            [ring.middleware.defaults :as ring-defaults]
-            [ring.util.codec :as codec]
-            [ring.middleware.session :as ring-session]
-            [ring.middleware.session.memory :as ring-session-memory]
-            [ring.middleware.session.cookie :as ring-session-cookie]
-            [active.clojure.config :as active-config]
             [active.clojure.oidc.config :as oidc-config]
+            [active.clojure.record :refer [define-record-type]]
+            [camel-snake-kebab.core :as csk]
             [clj-http.client :as http-client]
             [clojure.data.json :as json]
-            [camel-snake-kebab.core :as csk]))
+            [clojure.edn :as edn]
+            [clojure.string :as string]
+            [hiccup.page :as hp]
+            [reitit.ring :as rr]
+            [ring.adapter.jetty :as jetty]
+            [ring.middleware.cookies :as ring-cookies]
+            [ring.middleware.defaults :as ring-defaults]
+            [ring.middleware.session :as ring-session]
+            [ring.middleware.session.cookie :as ring-session-cookie]
+            [ring.middleware.session.memory :as ring-session-memory]
+            [ring.util.codec :as codec]
+            [ring.util.response :as response]))
 
 (def session-store (ring-session-memory/memory-store))
 
@@ -42,7 +45,7 @@
       (let [response
             (http-client/get (lens/yank oidc-profile (lens/>> oidc/oidc-profile-openid-provider-config
                                                               oidc/openid-provider-config-userinfo-endpoint))
-                                      {:headers {:authorization (str token-type " " token)}})]
+                             {:headers {:authorization (str token-type " " token)}})]
         (when (= (:status response) 200)
           (let [user-data (json/read-str (:body response) :key-fn csk/->kebab-case-keyword)]
             {:id     (:id user-data)
@@ -75,11 +78,15 @@
                                    [:p
                                     [:a {:href "/logout"} "Logout"]])]
                                 (mapv (fn [oidc-profile]
-                                        [:p
-                                         [:a {:href (oidc/oidc-profile-launch-uri oidc-profile)}
-                                          (str "Login via " (oidc/oidc-profile-name oidc-profile))]])
+                                        (let [check-session-iframe
+                                              (-> (oidc/oidc-profile-openid-provider-config oidc-profile)
+                                                  oidc/openid-provider-config-check-session-endpoint)]
+                                          [:div
+                                           [:p
+                                            [:a {:href (oidc/oidc-profile-launch-uri oidc-profile)}
+                                             (str "Login via " (oidc/oidc-profile-name oidc-profile))]]]))
                                       oidc-profiles))]])}
-          session (-> (:session req)
+          session   (-> (:session req)
                         (assoc :user-info user-info))]
       (-> resp
           (assoc :session session)))))
@@ -94,41 +101,41 @@
 
 (def not-found-handler
   (constantly
-    (response/not-found
-      (hp/html5
-        [:head
-         [:meta {:charset "UTF-8"}]]
-        [:body
-         [:h1 "Not found, sorry"]]))))
+   (response/not-found
+    (hp/html5
+     [:head
+      [:meta {:charset "UTF-8"}]]
+     [:body
+      [:h1 "Not found, sorry"]]))))
 
 (defn app
   [config]
-  (let [oidc-profiles (oidc/make-oidc-profiles config)]
-    (rr/ring-handler
+  (let [oidc-profiles (oidc/make-oidc-profiles! config)]
+    (rr/ring-handler 
      (rr/router
       (concat (oidc/reitit-routes oidc-profiles)
               [["/" {:get {:handler (fn [_] (response/redirect "/login"))}}]
                ["/login"  {:get {:handler (app-handler oidc-profiles)}}]
-               ["/logout" {:get {:handler (logout-handler (first oidc-profiles))}}]]))
-     not-found-handler
-     {:middleware [[ring-session/wrap-session (:session ring-config)]]})))
+               ["/logout" {:get {:handler (logout-handler (first oidc-profiles))}}]])
+      {:data {:middleware [[ring-session/wrap-session (:session ring-config)]
+                           [ring-cookies/wrap-cookies]]}}))))
 
 (def server (atom nil))
 
 (defn start-server
-  [config]
-  (reset! server (jetty/run-jetty (app config) {:host "0.0.0.0"
-                                                :port 1414
-                                                :join? false})))
+    [config]
+    (reset! server (jetty/run-jetty (app config) {:host "0.0.0.0"
+                                                 :port 1414
+                                                 :join? false})))
 
 (defn stop-server
-  []
-  (try
-    (.stop @server)
+    []
+    (try
+     (.stop @server)
     (catch Exception _
-      nil)
+       nil)
     (finally
-      (reset! server nil))))
+       (reset! server nil))))
 
 (defn run
   []
@@ -136,10 +143,10 @@
                     edn/read-string
                     (active-config/make-configuration (active-config/schema "The schema"
                                                                             oidc-config/section)
-                                                     []))]
+                                                      []))]
     (log4j/redirect-log4j!)
     (log/set-global-log-events-config-from-map! {:min-level :info})
     (start-server config)))
 
-;; (stop-server)
-;; (run)
+(run)
+(stop-server)
