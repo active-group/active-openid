15b14bfd849ba487db42dfaaff9d065d5f8f142e
Marco Schneider <marco.schneider@active-group.de>
2022-04-04 16:29:56 +0200

    The project is called active-openid.

--- src/active/clojure/openid.clj
+++ src/active/clojure/openid.clj
@@ -1,10 +1,10 @@
-(ns active.clojure.keycloak
+(ns active.clojure.openid
   (:require [active.clojure.condition :as condition]
             [active.clojure.config :as active-config]
             [active.clojure.config :as config]
             [active.clojure.lens :as lens]
-            [active.clojure.keycloak :as keycloak]
-            [active.clojure.keycloak.config :as keycloak-config]
+            [active.clojure.openid :as openid]
+            [active.clojure.openid.config :as openid-config]
             [active.clojure.record :refer [define-record-type]]
             [clj-http.client :as http-client]
             [clj-time.core :as time]
@@ -26,36 +26,36 @@
    check-session-endpoint openid-provider-config-check-session-endpoint
    supports-backchannel-logout? openid-provider-config-supports-backchannel-logout?])
 
-(define-record-type ^{:doc "Wraps all necessary information for a keycloak identity provider profile."}
-  KeycloakProfile
-  make-keycloak-profile keycloak-profile?
-  [name                   keycloak-profile-name
-   openid-provider-config keycloak-profile-openid-provider-config
-   client-id              keycloak-profile-client-id
-   client-secret          keycloak-profile-client-secret
-   scopes                 keycloak-profile-scopes
-   launch-uri             keycloak-profile-launch-uri
-   redirect-uri           keycloak-profile-redirect-uri
-   landing-uri            keycloak-profile-landing-uri
-   logout-uri             keycloak-profile-logout-uri
-   backchannel-logout-uri keycloak-profile-backchannel-logout-uri
-   basic-auth?            keycloak-profile-basic-auth?])
+(define-record-type ^{:doc "Wraps all necessary information for a openid identity provider profile."}
+  OpenidProfile
+  make-openid-profile openid-profile?
+  [name                   openid-profile-name
+   openid-provider-config openid-profile-openid-provider-config
+   client-id              openid-profile-client-id
+   client-secret          openid-profile-client-secret
+   scopes                 openid-profile-scopes
+   launch-uri             openid-profile-launch-uri
+   redirect-uri           openid-profile-redirect-uri
+   landing-uri            openid-profile-landing-uri
+   logout-uri             openid-profile-logout-uri
+   backchannel-logout-uri openid-profile-backchannel-logout-uri
+   basic-auth?            openid-profile-basic-auth?])
 
 (defn get-openid-configuration-url
   [scheme host port realm]
   (format "%s://%s:%d/auth/realms/%s/.well-known/openid-configuration" scheme host port realm))
 
-(defn- keycloak-supports-backchannel-logout?
-  ;; True if the `keycloak-profile` supports backchannel logouts as discovered
+(defn- openid-supports-backchannel-logout?
+  ;; True if the `openid-profile` supports backchannel logouts as discovered
   ;; on the .well-known page.
-  [keycloak-profile]
-  (lens/yank keycloak-profile (lens/>> keycloak-profile-openid-provider-config
+  [openid-profile]
+  (lens/yank openid-profile (lens/>> openid-profile-openid-provider-config
                                    openid-provider-config-supports-backchannel-logout?)))
 
-(define-record-type KeycloakInstanceNotAvailable
-  make-keycloak-instance-not-available keycloak-instance-not-available?
-  [tried-endpoint keycloak-instance-not-available-tried-instance
-   error-msg keycloak-instance-not-available-error-msg])
+(define-record-type OpenidInstanceNotAvailable
+  make-openid-instance-not-available openid-instance-not-available?
+  [tried-endpoint openid-instance-not-available-tried-instance
+   error-msg openid-instance-not-available-error-msg])
 
 (defn- get-openid-provider-config!
   ;; Based on the connection parameters, fetches the openid provider
@@ -63,8 +63,8 @@
 
   ;; Also see [here](https://ldapwiki.com/wiki/Openid-configuration).
 
-  ;; If the keycloak instance is not available, returns
-  ;; an [[%keycloak-instance-not-available]]] condition.
+  ;; If the openid instance is not available, returns
+  ;; an [[%openid-instance-not-available]]] condition.
   [scheme host port realm]
   (let [configuration-url (get-openid-configuration-url scheme host port realm)]
     (try (let [{:keys [status body]} (http-client/get configuration-url)]
@@ -78,23 +78,23 @@
                                                 (get json-map "backchannel_logout_supported")))
              "error"))
          (catch Exception e
-           (make-keycloak-instance-not-available configuration-url (.getMessage e))))))
+           (make-openid-instance-not-available configuration-url (.getMessage e))))))
 
-(defn make-keycloak-profiles!
+(defn make-openid-profiles!
   "Takes a [[active.clojure.config/Configuration]] and extracts all
-  configured [[KeycloakProfile]]s from the config.
+  configured [[OpenidProfile]]s from the config.
 
-  If any keycloak instance is not available, returns
-  an [[%keycloak-instance-not-available]]] condition instead of
-  an [[KeycloakProfile]] for that instance.."
+  If any openid instance is not available, returns
+  an [[%openid-instance-not-available]]] condition instead of
+  an [[OpenidProfile]] for that instance.."
   [config]
-  (let [keycloak-profiles-config (active-config/section-subconfig config keycloak-config/section)]
+  (let [openid-profiles-config (active-config/section-subconfig config openid-config/section)]
     (mapv (fn [c]
-            (let [scheme (active-config/access c keycloak-config/keycloak-scheme)
-                  host   (active-config/access c keycloak-config/keycloak-host)
-                  port   (active-config/access c keycloak-config/keycloak-port)
-                  realm  (active-config/access c keycloak-config/keycloak-realm)
-                  client (active-config/access c keycloak-config/keycloak-client)
+            (let [scheme (active-config/access c openid-config/openid-scheme)
+                  host   (active-config/access c openid-config/openid-host)
+                  port   (active-config/access c openid-config/openid-port)
+                  realm  (active-config/access c openid-config/openid-realm)
+                  client (active-config/access c openid-config/openid-client)
 
                   ;; This might fail Also, this might be a bad idea:
                   ;; TODO If the identity provider is unavailable at
@@ -103,38 +103,38 @@
                   (get-openid-provider-config! scheme host port realm)]
               (cond
                 (openid-provider-config? openid-provider-config-or-error)
-                (make-keycloak-profile (active-config/access c keycloak-config/keycloak-name)
+                (make-openid-profile (active-config/access c openid-config/openid-name)
                                    openid-provider-config-or-error
-                                   (active-config/access c keycloak-config/keycloak-client)
-                                   (active-config/access c keycloak-config/keycloak-client-secret)
-                                   (active-config/access c keycloak-config/keycloak-scopes)
-                                   (active-config/access c keycloak-config/keycloak-launch-uri)
-                                   (active-config/access c keycloak-config/keycloak-redirect-uri)
-                                   (active-config/access c keycloak-config/keycloak-landing-uri)
-                                   (active-config/access c keycloak-config/keycloak-logout-uri)
-                                   (active-config/access c keycloak-config/keycloak-backchannel-logout-uri)
-                                   (active-config/access c keycloak-config/keycloak-basic-auth?))
+                                   (active-config/access c openid-config/openid-client)
+                                   (active-config/access c openid-config/openid-client-secret)
+                                   (active-config/access c openid-config/openid-scopes)
+                                   (active-config/access c openid-config/openid-launch-uri)
+                                   (active-config/access c openid-config/openid-redirect-uri)
+                                   (active-config/access c openid-config/openid-landing-uri)
+                                   (active-config/access c openid-config/openid-logout-uri)
+                                   (active-config/access c openid-config/openid-backchannel-logout-uri)
+                                   (active-config/access c openid-config/openid-basic-auth?))
 
-                (keycloak-instance-not-available? openid-provider-config-or-error)
+                (openid-instance-not-available? openid-provider-config-or-error)
                 openid-provider-config-or-error)))
-          keycloak-profiles-config)))
+          openid-profiles-config)))
 
 (defn- join-scopes
   ;; Returns a string containing all configured
-  ;; [[keycloak-profile-scopes]], separated by `\space`.
-  [keycloak-profile]
-  (string/join " " (map name (keycloak-profile-scopes keycloak-profile))))
+  ;; [[openid-profile-scopes]], separated by `\space`.
+  [openid-profile]
+  (string/join " " (map name (openid-profile-scopes openid-profile))))
 
 (defn- authorize-uri
-  [keycloak-profile state]
-  (let [authorize-uri (lens/yank keycloak-profile (lens/>> keycloak-profile-openid-provider-config
+  [openid-profile state]
+  (let [authorize-uri (lens/yank openid-profile (lens/>> openid-profile-openid-provider-config
                                                        openid-provider-config-authorize-endpoint))]
     (str authorize-uri
          (if (string/includes? authorize-uri "?") "&" "?")
-         (let [redirect-uri (keycloak-profile-redirect-uri keycloak-profile)]
+         (let [redirect-uri (openid-profile-redirect-uri openid-profile)]
            (codec/form-encode {:response_type "code"
-                               :client_id     (keycloak-profile-client-id keycloak-profile)
-                               :redirect_uri  (keycloak-profile-redirect-uri keycloak-profile)
+                               :client_id     (openid-profile-client-id openid-profile)
+                               :redirect_uri  (openid-profile-redirect-uri openid-profile)
                                :state         state})))))
 
 (defn- random-state
@@ -144,11 +144,11 @@
       (string/replace "/" "_")))
 
 (defn- make-launch-handler
-  [keycloak-profile]
+  [openid-profile]
   (fn [request]
     (let [state       (random-state)
           new-session (assoc (:session request) ::authorize-state state)]
-      (-> (response/redirect (authorize-uri keycloak-profile state))
+      (-> (response/redirect (authorize-uri openid-profile state))
           (assoc :session new-session)))))
 
 (defn- coerce-to-int [n]
@@ -172,10 +172,10 @@
   (get-in request [:query-params "code"]))
 
 (defn- request-params
-  [keycloak-profile request]
+  [openid-profile request]
   {:grant_type   "authorization_code"
    :code         (get-authorization-code request)
-   :redirect_uri (keycloak-profile-redirect-uri keycloak-profile)})
+   :redirect_uri (openid-profile-redirect-uri openid-profile)})
 
 (defn- add-header-credentials
   [options client-id client-secret]
@@ -188,19 +188,19 @@
                                           :client_secret client-secret}))))
 
 (defn- get-access-token
-  "For a `keycloak-profile` and based on a `request` (the response of the
+  "For a `openid-profile` and based on a `request` (the response of the
   idp), fetch the actual (JWT) access token.
 
   Might throw an exception."
-  [keycloak-profile request]
-  (let [access-token-uri (lens/yank keycloak-profile (lens/>> keycloak-profile-openid-provider-config
+  [openid-profile request]
+  (let [access-token-uri (lens/yank openid-profile (lens/>> openid-profile-openid-provider-config
                                                           openid-provider-config-token-endpoint))
-        client-id        (keycloak-profile-client-id keycloak-profile)
-        client-secret    (keycloak-profile-client-secret keycloak-profile)
-        basic-auth?      (keycloak-profile-basic-auth? keycloak-profile)]
+        client-id        (openid-profile-client-id openid-profile)
+        client-secret    (openid-profile-client-secret openid-profile)
+        basic-auth?      (openid-profile-basic-auth? openid-profile)]
     (let [resp (http-client/post access-token-uri
                                  (cond-> {:accept :json, :as :json,
-                                          :form-params (request-params keycloak-profile request)}
+                                          :form-params (request-params openid-profile request)}
                                    basic-auth?       (add-header-credentials client-id client-secret)
                                    (not basic-auth?) (add-form-credentials client-id client-secret)))]
       (format-access-token resp))))
@@ -219,12 +219,12 @@
 (def ^:private default-no-auth-code-handler (constantly no-auth-code-response))
 
 (defn- make-redirect-handler
-  ;; Creates a redirect (callback) handler for a `keycloak-profile`.  A
+  ;; Creates a redirect (callback) handler for a `openid-profile`.  A
   ;; successful login might result in an exceptional state (i.e. when
   ;; the server cannot be reached after receiving the code.  Such
   ;; errors will be returned as a ring-response with code 500 and the
   ;; class and message as a Clojure-map.
-  [keycloak-profile no-auth-code-handler state-mismatch-handler]
+  [openid-profile no-auth-code-handler state-mismatch-handler]
   (fn [{:keys [session] :as request}]
     (cond
       (not (state-matches? request))
@@ -235,10 +235,10 @@
 
       :else
       (try 
-        (let [access-token (get-access-token keycloak-profile request)
-              resp (-> (response/redirect (keycloak-profile-landing-uri keycloak-profile))
+        (let [access-token (get-access-token openid-profile request)
+              resp (-> (response/redirect (openid-profile-landing-uri openid-profile))
                        (assoc :session (-> session
-                                           (assoc-in [::access-tokens (keycloak-profile-name keycloak-profile)] access-token)
+                                           (assoc-in [::access-tokens (openid-profile-name openid-profile)] access-token)
                                            (dissoc ::authorize-state))))]
           resp)
         (catch Exception e
@@ -248,49 +248,49 @@
               (response/header "Content-Type" "application/json")))))))
 
 (defn- make-user-session-destroyer
-  [keycloak-profile]
+  [openid-profile]
   (fn [req]
-    (update-in req [:session ::access-tokens] dissoc (keycloak-profile-name keycloak-profile))))
+    (update-in req [:session ::access-tokens] dissoc (openid-profile-name openid-profile))))
 
-(defn keycloak-logout
+(defn openid-logout
   "Function that performs a logout at the idp for the current user.
-  Clears the whole :session for the `keycloak-profile`."
-  [keycloak-profile]
+  Clears the whole :session for the `openid-profile`."
+  [openid-profile]
   (let [end-session-endpoint
-        (lens/yank keycloak-profile (lens/>> keycloak-profile-openid-provider-config
+        (lens/yank openid-profile (lens/>> openid-profile-openid-provider-config
                                          openid-provider-config-end-session-endpoint))
-        destroy-user-session (make-user-session-destroyer keycloak-profile)]
+        destroy-user-session (make-user-session-destroyer openid-profile)]
     (-> (response/redirect
          (str end-session-endpoint
               "?"
               (codec/form-encode {:post_logout_redirect_uri (str "http://localhost:1414"
-                                                                 (keycloak-profile-landing-uri keycloak-profile))})))
+                                                                 (openid-profile-landing-uri openid-profile))})))
         destroy-user-session)))
 
 (defn make-backchannel-logout-handler
-  [keycloak-profile]
+  [openid-profile]
   (fn [req]
     (println "backchannel-logout-handler" req)
-    ((make-user-session-destroyer keycloak-profile) req)))
+    ((make-user-session-destroyer openid-profile) req)))
 
 (defn reitit-routes-for-profile
-  "For a given [[KeycloakProfile]], returns a vector containing the launch-
+  "For a given [[OpenidProfile]], returns a vector containing the launch-
   and login-callback handlers."
-  [keycloak-profile no-auth-code-handler state-mismatch-handler]
-  (->> (concat [[(keycloak-profile-launch-uri keycloak-profile)
-                 {:get {:handler (make-launch-handler keycloak-profile)}}]
-                [(keycloak-profile-redirect-uri keycloak-profile)
-                 {:get {:handler    (make-redirect-handler keycloak-profile no-auth-code-handler state-mismatch-handler)
+  [openid-profile no-auth-code-handler state-mismatch-handler]
+  (->> (concat [[(openid-profile-launch-uri openid-profile)
+                 {:get {:handler (make-launch-handler openid-profile)}}]
+                [(openid-profile-redirect-uri openid-profile)
+                 {:get {:handler    (make-redirect-handler openid-profile no-auth-code-handler state-mismatch-handler)
                         :middleware [[wrap-params]]}}]]
-               (when (keycloak-supports-backchannel-logout? keycloak-profile)
-                 [[(keycloak-profile-logout-uri keycloak-profile)
-                   {:post {:handler    (make-backchannel-logout-handler keycloak-profile)
+               (when (openid-supports-backchannel-logout? openid-profile)
+                 [[(openid-profile-logout-uri openid-profile)
+                   {:post {:handler    (make-backchannel-logout-handler openid-profile)
                            :middleware [[wrap-params]]}}]]))
        (into [])))
 
 (defn reitit-routes
-  "Based on a sequence of [[KeycloakProfile]]s, returns a vector of two
-  reitit routes that handle the initial login launch and the keycloak
+  "Based on a sequence of [[OpenidProfile]]s, returns a vector of two
+  reitit routes that handle the initial login launch and the openid
   callback.
   Takes an optional map with up to two keys
 
@@ -311,17 +311,17 @@
   1. A valid login: The login was successful.  The callback handler
   will use the code provided by the idp and fetches an access token (a
   JWT token).  The token will be assed to the session under
-  `[::access-tokens <keycloak-profile-name> <access-token>]`.
+  `[::access-tokens <openid-profile-name> <access-token>]`.
 
   2. The idp didn't provide an authorization code.  The callback
   handler returns the [[no-auth-code-response]].
 
   3. The state code's did not match.  The callback handle rreturns
   the [[state-mismatch-response]]."
-  [keycloak-profiles & [{:keys [no-auth-code-handler
+  [openid-profiles & [{:keys [no-auth-code-handler
                             state-mismatch-handler]
                      :or   {no-auth-code-handler   default-no-auth-code-handler
                             state-mismatch-handler default-state-mismatch-handler}}]]
-  (into [] (mapcat (fn [keycloak-profile]
-                     (reitit-routes-for-profile keycloak-profile no-auth-code-handler state-mismatch-handler))
-                   keycloak-profiles)))
+  (into [] (mapcat (fn [openid-profile]
+                     (reitit-routes-for-profile openid-profile no-auth-code-handler state-mismatch-handler))
+                   openid-profiles)))
--- src/active/clojure/openid/config.clj
+++ src/active/clojure/openid/config.clj
@@ -1,113 +1,113 @@
-(ns active.clojure.keycloak.config
-  "Configuration schema for Keycloak keycloak identity providers."
+(ns active.clojure.openid.config
+  "Configuration schema for OpenID identity providers."
   (:require [active.clojure.config :as config]))
 
-;; keycloak
-(def keycloak-name
+;; openid
+(def openid-name
   (config/setting
    :name
    "The name of the profile."
    config/string-range))
 
-(def keycloak-host
+(def openid-host
   (config/setting
    :host
-   "The host the keycloak service is hosted on."
+   "The host the openid service is hosted on."
    config/string-range))
 
-(def keycloak-scheme
+(def openid-scheme
   (config/setting
    :scheme
    "The scheme to use to connect to the idp."
    (config/one-of-range #{"http" "https"} "http")))
 
-(def keycloak-port
+(def openid-port
   (config/setting
    :port
-   "The port the keycloak service is listening on."
+   "The port the openid service is listening on."
    (config/integer-between-range 0 65534 1553)))
 
-(def keycloak-realm
+(def openid-realm
   (config/setting
    :realm
    "The realm PhoenixNG belongs to."
    (config/default-string-range "rtp1")))
 
-(def keycloak-client
+(def openid-client
   (config/setting
    :client
-   "A string that identifies PhoenixNG at the keycloak identity provider."
+   "A string that identifies PhoenixNG at the openid identity provider."
    (config/default-string-range "phoenix-ng")))
 
-(def keycloak-client-secret
+(def openid-client-secret
   (config/setting
    :client-secret
    "The secret that acts as a password for the `:client` at the idp."
    config/string-range))
 
-(def keycloak-scopes
+(def openid-scopes
   (config/setting
    :scopes
    "The scopes to fetch from the idp."
    (config/sequence-of-range config/string-range)))
 
-(def keycloak-launch-uri
+(def openid-launch-uri
   (config/setting
    :launch-uri
    "The relative uri that initiates the authentication process."
    config/string-range))
 
-(def keycloak-redirect-uri
+(def openid-redirect-uri
   (config/setting
    :redirect-uri
    "The relative uri that serves as the callback endpoint to the authentication process."
    config/string-range))
 
-(def keycloak-landing-uri
+(def openid-landing-uri
   (config/setting
    :landing-uri
    "The relative uri a user will be redirected to after a logout."
    config/string-range))
 
-(def keycloak-logout-uri
+(def openid-logout-uri
   (config/setting
    :logout-uri
    "The relative uri that serves as the logout endpoint."
    config/string-range))
 
-(def keycloak-backchannel-logout-uri
+(def openid-backchannel-logout-uri
   (config/setting
    :backchannel-logout-uri
    "The relative uri that serves as the backchannel logout endpoint."
    config/string-range))
 
-(def keycloak-basic-auth?
+(def openid-basic-auth?
   (config/setting
    :basic-auth?
    "???"
    (config/boolean-range false)))
 
-(def keycloak-schema
+(def openid-schema
   (config/schema "Configuration schema for the openid identity provider"
-                 keycloak-name
-                 keycloak-host
-                 keycloak-port
-                 keycloak-scheme
-                 keycloak-realm
-                 keycloak-client
-                 keycloak-client-secret
-                 keycloak-scopes
-                 keycloak-launch-uri
-                 keycloak-redirect-uri
-                 keycloak-landing-uri
-                 keycloak-logout-uri
-                 keycloak-backchannel-logout-uri
-                 keycloak-basic-auth?))
+                 openid-name
+                 openid-host
+                 openid-port
+                 openid-scheme
+                 openid-realm
+                 openid-client
+                 openid-client-secret
+                 openid-scopes
+                 openid-launch-uri
+                 openid-redirect-uri
+                 openid-landing-uri
+                 openid-logout-uri
+                 openid-backchannel-logout-uri
+                 openid-basic-auth?))
 
-(def keycloak-sequence-schema
+(def openid-sequence-schema
   (config/sequence-schema
-   "A sequence of keycloak identity provider configurations."
-   keycloak-schema))
+   "A sequence of openid identity provider configurations."
+   openid-schema))
 
 (def section
-  (config/section :keycloak keycloak-sequence-schema))
+  (config/section :openid openid-sequence-schema))
