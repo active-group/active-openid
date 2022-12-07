6a1ddfbb63c12dff5e6221fccc916f03365d9af2
Tobias Paulus <tobias.paulus@active-group.de>
2022-12-07 14:39:25 +0100

    Tests for absolute-redirect-uri.
    Also test prefixed uris seperately.

--- test/active/clojure/openid_test.clj
+++ test/active/clojure/openid_test.clj
@@ -14,7 +14,8 @@
      :client        {:id          "openid-test"
                      :secret      "<redacted>"
                      :scopes      ["openid" "username" "email"]
-                     :basic-auth? true}
+                     :basic-auth? true
+                     :base-uri    "http://localhost:8888"}
      :callback-uris {:launch-uri   "/auth/login"
                      :redirect-uri "/auth/login-callback"
                      :landing-uri  "/login"
@@ -38,6 +39,25 @@
                                "client-id"
                                "client-secret"
                                ["scope"]
+                               "http://localhost:8888"
+                               "/login"
+                               "/login-callback"
+                               "/"
+                               "/logout"
+                               false)
+   (openid/make-openid-profile "profile-name"
+                               ""
+                               (openid/make-openid-provider-config
+                                "host:port/auth"
+                                "host:port/token"
+                                "host:port/userinfo"
+                                "host:port/end_session"
+                                "host:port/check_session_iframe"
+                                false)
+                               "client-id"
+                               "client-secret"
+                               ["scope"]
+                               "http://localhost:8888"
                                "/login"
                                "/login-callback"
                                "/"
@@ -47,8 +67,20 @@
 (t/deftest launch-uri-test
   (t/is (= "/profile-prefix/login" (openid/launch-uri (first openid-profiles)))))
 
+(t/deftest absolute-redirect-uri-test
+  (t/is (= "http://localhost:8888/profile-prefix/login-callback" (openid/absolute-redirect-uri (first openid-profiles)))))
+
 (t/deftest redirect-uri-test
   (t/is (= "/profile-prefix/login-callback" (openid/redirect-uri (first openid-profiles)))))
+
+(t/deftest launch-uri-test-without-prefix
+  (t/is (= "/login" (openid/launch-uri (second openid-profiles)))))
+
+(t/deftest absolute-redirect-uri-test-without-prefix
+  (t/is (= "http://localhost:8888/login-callback" (openid/absolute-redirect-uri (second openid-profiles)))))
+
+(t/deftest redirect-uri-test-without-prefix
+  (t/is (= "/login-callback" (openid/redirect-uri (second openid-profiles)))))
 
 (t/deftest req->access-tokens-test
   (t/testing "with no access tokens, returns nil"
