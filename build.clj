d14c7256df97565e336cb697de08d45c159fdd6d
Marcus Crestani <marcus.crestani@active-group.de>
2023-03-14 12:28:55 +0100

    Move away from versioning with counting git commits.

--- build.clj
+++ build.clj
@@ -4,7 +4,7 @@
             [deps-deploy.deps-deploy    :as dd]))
 
 
-(def -version [0 1 (b/git-count-revs nil)])
+(def -version [0 1 105])
 
 (let [[major minor patch] -version]
   (def release-version (format "%s.%s.%s" major minor patch))
