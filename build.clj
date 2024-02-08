b2aaaa9bb160f05333399a2f2316ad36453336e9
Marcus Crestani <marcus.crestani@active-group.de>
2024-02-08 09:30:56 +0100

    Release version 0.2.2.

--- build.clj
+++ build.clj
@@ -4,7 +4,7 @@
             [deps-deploy.deps-deploy    :as dd]))
 
 
-(def -version [0 2 1])
+(def -version [0 2 2])
 
 (let [[major minor patch] -version]
   (def release-version (format "%s.%s.%s" major minor patch))
